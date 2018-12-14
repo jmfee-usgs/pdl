0e8be2471767e4dd7f08e267bd5744755d4a4228
Jeremy Fee <jmfee@usgs.gov>
2018-12-14 10:24:48 -0700

    Add configuration support for custom websocket port, and add to docs

--- src/main/java/gov/usgs/earthquake/indexer/WebSocketIndexerListener.java
+++ src/main/java/gov/usgs/earthquake/indexer/WebSocketIndexerListener.java
@@ -1,12 +1,19 @@
 package gov.usgs.earthquake.indexer;
 
-import gov.usgs.util.WebSocketServer;
+import java.util.logging.Logger;
 
+import gov.usgs.util.Config;
+import gov.usgs.util.WebSocketServer;
 
 /**
  * Listener that broadcasts indexer events over a websocket.
  */
 public class WebSocketIndexerListener extends DefaultIndexerListener {
+    /** Logging object. */
+    private final Logger LOGGER = Logger.getLogger(WebSocketIndexerListener.class.getName());
+
+    /** Configuration property for port. */
+    private static final String PORT_PROPERTY = "port";
 
     /** Websocket for broadcast. */
     private WebSocketServer webSocket;
@@ -14,13 +21,19 @@ public class WebSocketIndexerListener extends DefaultIndexerListener {
     /** Default websocket port. */
     private int port = 8080;
 
-
     /**
      * Override the IndexerListener to broadcast the event.
      */
     @Override
     public void onIndexerEvent(IndexerEvent event) throws Exception {
         webSocket.broadcast(new JsonEventSummary(event).toJsonObject().toString());
+    }
+
+    @Override
+    public void configure(final Config config) {
+        String portValue = config.getProperty(PORT_PROPERTY, "8080");
+        this.port = Integer.valueOf(portValue);
+        LOGGER.config("[" + getName() + "] web socket port " + this.port);
     }
 
     /**
@@ -42,7 +55,7 @@ public class WebSocketIndexerListener extends DefaultIndexerListener {
         if (webSocket != null) {
             webSocket.stop();
             webSocket = null;
-        }   
+        }
     }
 
 }
